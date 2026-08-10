43501121fe7bb4f3a60de623d31594b424017fb3
Priyangshu Majumder <ritamvision@gmail.com>
2026-08-10 22:23:36 +0530

    Time: 10 ms (43.55%) | Memory: 13.7 MB (16.58%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,21 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        seen={}
+        indexed_nums = []
         for i in range(len(nums)):
-            complement=target-nums[i]
-            if complement in seen:
-                return [seen[complement],i]
-            seen[nums[i]]=i
-        
+            indexed_nums.append((nums[i], i))
+
+        indexed_nums.sort()
+
+        left = 0
+        right = len(indexed_nums) - 1
+
+        while left < right:
+            total = indexed_nums[left][0] + indexed_nums[right][0]
+            if total == target:
+                return [indexed_nums[left][1], indexed_nums[right][1]]
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+
+        return []

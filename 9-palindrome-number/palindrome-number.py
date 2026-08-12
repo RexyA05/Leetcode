775c3d1f96a5c92a3c88a7a47d939628df80633e
Priyangshu Majumder <ritamvision@gmail.com>
2026-08-12 22:46:13 +0530

    Time: 5 ms (84.45%) | Memory: 12.4 MB (55.34%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        original=x
+        rev=0
+        if x<0:
+            return False
+        s=str(x)
+        left, right=0,len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True            
+
+        
